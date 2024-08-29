3b8b7b3c091db602c8153a242dfec6dc73a37c4d
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-08-29 08:47:43 +0530

    login start

--- src/login.js
+++ src/login.js
@@ -1,7 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 export default class login extends Component {
+
+  const username = (setUsername)
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1 className="header">Login to your existing account</h1>
+        <form action="submit">
+          <input type="text" placeholder="username" />
+        </form>
+      </div>
+    );
   }
 }
