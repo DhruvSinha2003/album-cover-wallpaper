3290b6c2b9fc3389d7255cef8e8f81aba51f90c9
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-08-30 22:13:02 +0530

    login

--- src/login.js
+++ src/login.js
@@ -2,13 +2,18 @@ import React, { Component, useState } from "react";
 
 export default class login extends Component {
 
-  const username = (setUsername)
-  render() {
+  function submit(){
+    setUsername()
+  }
+
+  const [username, setUsername] = useState("hello");
+    render() {
     return (
       <div>
         <h1 className="header">Login to your existing account</h1>
         <form action="submit">
-          <input type="text" placeholder="username" />
+          <input type="text" placeholder="username" value={username} />
+          <button onClick={submit()}>Submit</button>
         </form>
       </div>
     );
